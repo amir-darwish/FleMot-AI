e9c3eeaffd981a7f1ee603f26be68ebd83eb14de
Amir <mohamed.amir.darwish@gmail.com>
2025-09-16 13:45:24 +0200

    Intégrer le SDK Firebase et la logique de connexion

--- backend/FleMot.Api/Program.cs
+++ backend/FleMot.Api/Program.cs
@@ -1,11 +1,52 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using MongoDB.Driver;
+using FleMot.Api.DataAccess;
+using FleMot.Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(); 
+builder.Services.AddSwaggerGen();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var firebaseProjectId = "flemotv2"; 
+        options.Authority = $"https://securetoken.google.com/{firebaseProjectId}";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = $"https://securetoken.google.com/{firebaseProjectId}",
+            ValidateAudience = true,
+            ValidAudience = firebaseProjectId,
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.FromMinutes(5) 
+        };
+
+        options.Events = new JwtBearerEvents
+        {
+            OnAuthenticationFailed = context =>
+            {
+                Console.WriteLine("--- AUTHENTICATION FAILED ---");
+                Console.WriteLine(context.Exception.ToString());
+                return Task.CompletedTask;
+            },
+            OnTokenValidated = context => {
+                Console.WriteLine("--- TOKEN VALIDATED SUCCESSFULLY ---");
+                return Task.CompletedTask;
+            }
+        };
+    });
+
+
+builder.Services.AddSingleton<IMongoClient>(s => 
+    new MongoClient(builder.Configuration.GetValue<string>("MongoDbSettings:ConnectionString")));
+builder.Services.AddScoped<IMongoDatabase>(s => 
+    s.GetRequiredService<IMongoClient>().GetDatabase(builder.Configuration.GetValue<string>("MongoDbSettings:DatabaseName")));
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 
 var app = builder.Build();
@@ -13,17 +54,14 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "FleMot API V1");
-        options.RoutePrefix = "swagger"; 
-    });
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FleMot API v1"));
 }
 
 
+app.UseRouting(); 
 
-app.UseAuthentication();
-app.UseAuthorization();
+app.UseAuthentication(); 
+app.UseAuthorization();  
 
 app.MapControllers();
 
